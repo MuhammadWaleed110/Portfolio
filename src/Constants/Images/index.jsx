72c86cd8fc6fce42ba1575af44585c0dae4892be
Muhammad Waleed <mw051878@gmail.com>
2025-12-23 23:15:55 +0500

    Initial commit - Portfolio project

---
+++ src/Constants/Images/index.jsx
@@ -0,0 +1,13 @@
+const profile_Image = "/public/Profile-Image.png";
+const airport_Parking = "/public/ISB-AIRPORT-PARKING.png";
+const coffee_khaas = "/public/Coffee_Khaas.png";
+const blood_connect = "/public/blood_donation.png"
+
+const Images = {
+    profile_Image,
+    airport_Parking,
+    coffee_khaas,
+    blood_connect,
+};
+
+export default Images;
